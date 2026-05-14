9d5894dd3521c1579848178cb2fe7c5e71a88e5c
khairul islam <khairulsagor567@gmail.com>
2026-05-14 23:40:20 +0600

    create a dev branch and added 19th array function problem

--- app.js
+++ app.js
@@ -427,3 +427,26 @@ const countCharacter = str.split('').reduce((acc, char) => {
   return acc;
 },{});
 //console.log(countCharacter); // Output: {H:1, e:1, l: 2, o:1}
+
+/*
+19. Last Elements of Array
+
+Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array.
+
+Test Data :
+
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2],3));
+console.log(last([7, 9, 0, -2],6));
+
+Expected Output :
+-2
+[9, 0, -2]
+[7, 9, 0, -2]*/
+
+function last(arr)
+{
+   return arr[arr.length-1];
+}
+
+console.log(last([7, 9, 0, -2]));
